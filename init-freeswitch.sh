ea70faeda72a8471a572aa0f334eeb5dcbd9b1e2
root <aleckyj@hotmail.com>
2021-06-24 10:15:57 +0800

    修正配置脚本

--- init-freeswitch.sh
+++ init-freeswitch.sh
@@ -4,8 +4,9 @@
 # $3 docker 挂载的/etc目录，用于配置odbc.ini
 # change docker ip from input args
 if [ ! -n "$1" ] ;then
-  sed -i "s/36.133.135.4/$1/g" ./freeswitch_config/vars.xml
-  sed -i "s/36.133.135.4/$1/g" ./freeswitch_config/internal.xml
+  sed -i "s/36.133.135.4/$1/g" ./freeswitch_conf/vars.xml
+  sed -i "s/36.133.135.4/$1/g" ./freeswitch_conf/internal.xml
+  sed -i "s/36.133.135.4/$1/g" ./freeswitch_conf/external.xml
   echo "已替换为指定的IP地址:$1"
 fi
 # copy config file to docker 
@@ -18,10 +19,10 @@ then
   cp ./freeswitch_conf/db.conf.xml $2"/autoload_configs"
   cp ./freeswitch_conf/event_socket.conf.xml $2"/autoload_configs"
   cp ./freeswitch_conf/switch.conf.xml $2"/autoload_configs"
+  cp ./freeswitch_conf/modules.conf.xml $2"/autoload_configs"
   cp ./freeswitch_conf/*ternal.xml $2"/sip_profiles"
   echo "初始化已完成，请重新加载docker restart freeswitch"
 else
   echo "配置目录不合法:$2"
 fi
-cp 
 
